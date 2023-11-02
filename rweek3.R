945d59bc66b17cb3ba7c4b619dd2be4da937042b
Nisaki <isaki.naoshi@gmail.com>
2023-11-02 16:38:26 +0900

    modify week3

--- rweek3.R
+++ rweek3.R
@@ -12,6 +12,8 @@ knitr::opts_chunk$set(echo = TRUE)
 coin <- c("Head","Tail")
 sample(coin, 100, replace = TRUE) #sample(標本/範囲, 抽出個数) #, replace = TRUE 復元抽出
 
+
+## ------------------------------------------------------------------------
 #コイン投げのシミュレーション
 #P(A)=100!/50!50!=1/2^100
 factorial(100)/factorial(50)^2/2^100 #factorial(), 階乗
@@ -29,21 +31,37 @@ for(i in 1:S){ #forループ
   z <- sample(coin, 100, replace = TRUE) 
   rec[i]<-sum(z)
 }
-hist(rec, main = "コイン投げ100回(表)", xlab = "表の出た回数")
+hist(rec, main = "コイン投げ100回(表)", 
+     xlab = "表の出た回数", ylab = "頻度")
 summary(rec)
 
 
 ## ------------------------------------------------------------------------
 #おまけ
+hist1 <- hist(rec, plot = FALSE)
+hist1_grob <- ggplotGrob(hist1)
+hist1_grob <- arrangeGrob(hist1_grob, 
+                          top = textGrob("コイン投げ100回(表)"), 
+                          left = textGrob("表の出た回数"), 
+                          bottom = textGrob("頻度"))
+install.packages("cowplot")
+
 library(ggplot2)
 REC <- as.data.frame(rec)
-ggplot(REC, aes(rec)) +
-  geom_histogram()
-hist(rec, main = "コイン投げ100回(表)", xlab = "表の出た回数")
-ggplot(REC,aes(rec)) +
-  geom_histogram()
-ggplot(REC,aes(rec)) +
-  geom_histogram(binwidth = 2)
+hist2 <- ggplot(REC, aes(rec)) +
+          geom_histogram() +
+          labs(title = "コイン投げ100回(表)", x = "表の出た回数", y = "頻度") +
+          theme_bw(base_family = "family_sans") +
+          theme(plot.title = element_text(hjust = 0.5))
+grid.arrange(hist1_grob, hist2, ncol = 2)
+
+#bin幅指定
+hist3 <- ggplot(REC, aes(rec)) +
+          geom_histogram(binwidth = 2) +
+          labs(title = "コイン投げ100回(表)", x = "表の出た回数", y = "頻度") +
+          theme_bw(base_family = "family_sans") +
+          theme(plot.title = element_text(hjust = 0.5))
+gridExtra::grid.arrange(hist1, hist3, nrow = 1)
 
 
 ## ------------------------------------------------------------------------
@@ -52,13 +70,14 @@ ggplot(REC,aes(rec)) +
 (200==100*2) #"="ではなく"=="
 TRUE + TRUE #TRUE=1, FALSE=0
 TRUE + FALSE #TRUE=1,FALSE=0
-count <- (rec==50) #ちょうど50回表が出た
+count <- (rec==50) #ちょうど50回表が出た場合
 sum(count)
 mean(count) #相対頻度
 #理論値とおよそ同じ
 
 
 ## ------------------------------------------------------------------------
+#独立性の確認用スクリプト
 S <- 100000	#シミュレーション回数
 X <- rnorm(S, 50, 10)	#Xを抽出
 Y <- rnorm(S, 50, 10)	#Yを抽出
@@ -101,19 +120,17 @@ head(malesdata)
 summary(malesdata)
 plot(malesdata)
 sch12 <- malesdata[malesdata$school==12, ] #高校卒業者の抜き出し
-malesdata$school
-malesdata$exper
-mean(sch12$wage)
 sch16 <- malesdata[malesdata$school==16, ] #学部卒業者の抜き出し
 exp(mean(sch12$wage)) #高校卒業者の平均賃金
 exp(mean(sch16$wage)) #学部卒業者の平均賃金
 #学部卒業者よりも高校卒業者の平均賃金が高くなっている
-#正しい推定
+
 malesdata <- read.csv("wage.csv")
-sch11 <- malesdata[malesdata$school==11, ] #高校未満の抜き出し
+sch11 <- malesdata[malesdata$school==11, ] #高卒未満の抜き出し
 sch12 <- malesdata[malesdata$school>=12, ] #高卒以上の抜き出し
 exp(mean(sch11$wage)) #高卒未満の平均賃金
 exp(mean(sch12$wage)) #高卒以上の平均賃金
+#正しい推定
 
 
 ## ------------------------------------------------------------------------
@@ -127,7 +144,7 @@ n <- 10000 #標本の大きさ
 
 Zn <- numeric(S) #結果記録用のリスト
 for(i in 1:S){ #繰り返し開始
-  X <- rnorm(n,50,10)	#N(50,10)から標本抽出
+  X <- rnorm(n, 50, 10)	#N(50,10)から標本抽出
   Xbar <- mean(X)
   Sn <- var(X)
   Zn[i] <- sqrt(n)*(Xbar-50)/sqrt(Sn)
@@ -154,47 +171,45 @@ pnorm(50 + (1.96*10), 50, 10) - pnorm(50 - (1.96*10), 50, 10)
 
 
 ## ------------------------------------------------------------------------
-read.csv("temperature.csv")
 data <- read.csv("temperature.csv")
 
-#3.1.1.
+
+## ------------------------------------------------------------------------
 mean(data$temp) #気温平均
 
-#3.1.2.
+
+## ------------------------------------------------------------------------
 mean(data$temp[1:100]) #気温平均（100番目まで）
 mean(data$temp); mean(data$temp[1:100]) #1と比較
 
-#3.1.3.
+
+## ------------------------------------------------------------------------
 sample(data$temp, 100) #無作為抽出
 mean(sample(data$temp, 100))
 mean(data$temp); mean(sample(data$temp, 100)) #1と比較
 
-#3.1.4.
-#違いがなぜ生じたのか
+
+## ------------------------------------------------------------------------
+data2 <- read.csv("icecream.csv", fileEncoding = "utf8")
+max(data2$icecream)
+which.max(data2$icecream)
+data2$city[which.max(data2$icecream)] #17番目は富山市
 
 
 ## ------------------------------------------------------------------------
-#3.2.1.
-data2 <- read.csv("icecream.csv")
-max(data2$icecream)
-which.max(data2$icecream)
-data2 #17番目は富山市
-
-#3.2.2.
 x <- data2$icecream
 y <- data2$income
 plot(x, y, main = "Figure 3.2.2.")
 cor(x, y)
 
-#3.2.3
+
+## ------------------------------------------------------------------------
 z <- data2$u15
 plot(x, z, main = "Figure 3.2.3.")
 cor(x, z)
 
-#3.2.4
-#略
 
-#3.3.1
+## ------------------------------------------------------------------------
 S <- 1000
 X <- rnorm(S, 50, 10)
 rec<-numeric(S)
@@ -203,7 +218,8 @@ for(i in 1:S){
 }
 mean(rec)
 
-#3.3.2. 
+
+## ------------------------------------------------------------------------
 S <- 1000
 X <- rnorm(S, 50, 10)
 rec <- numeric(S)
@@ -212,7 +228,8 @@ for(i in 1:S){
 }
 mean(rec)
 
-#3.3.3
+
+## ------------------------------------------------------------------------
 S <- 1000
 X <- rnorm(S, 50, 10)
 Y <- rnorm(S, 50, 10)
@@ -222,15 +239,48 @@ for(i in 1:S){
 }
 mean(rec)
 
-#3.4.
+
+## ------------------------------------------------------------------------
+S <- 10000
+n <- 10000
+X <- rnorm(n, 50, 10)
+Y <- rnorm(n, 50, 10)
+rec <- numeric(S)
+for(i in 1:S){
+    rec[i] <- (10 < X[i])
+}
+mean(rec)
+#recの中身
+#X[1]
+#10 < X[i]
+#rec[1] <- 10 < X[i]
+#rec[1]
+
+
+## ------------------------------------------------------------------------
+for(i in 1:S){
+    rec[i] <- (-10 < X[i]) & (X[i] < 10)
+}
+mean(rec)
+
+
+## ------------------------------------------------------------------------
+for(i in 1:S){
+    rec[i] <- (X[i] > Y[i]^2)
+}
+mean(rec)
+
+
+## ------------------------------------------------------------------------
 S <- 10000
 n <- 10000
 rec <- numeric(S)
 for(i in 1:S){
   X <- rnorm(n, 50, 10)
   Xbar <- mean(X)
+  Vn <- var(X)
   Sn <- var(X)
-  rec[i] <- (Xbar-1.64*sqrt(Sn/n)<50)&(50<Xbar+1.64*sqrt(Sn/n))
+  rec[i] <- (Xbar - 1.64*sqrt(Sn/n) <50)&(50 < Xbar + 1.64*sqrt(Sn/n))
 }
 mean(rec)
 
